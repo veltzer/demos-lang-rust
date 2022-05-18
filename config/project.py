fb03c055460755f6c1c84462acb602ad5687dee5
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:58 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "demos-rust"
-project_long_description = "Demos for the rust language"
-project_year_started = 2020
-project_description = """Demos for the rust language"""
+description_short = "Demos for the rust language"
