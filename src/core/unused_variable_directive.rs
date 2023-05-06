bcbbc8a34b1ef790a1753926335448cb6f4f2bf5
Mark Veltzer <mark.veltzer@gmail.com>
2023-05-06 09:49:03 +0300

    more examples

---
+++ src/core/unused_variable_directive.rs
@@ -0,0 +1,11 @@
+#![allow(unused)]
+
+// This is an example of an unused variabe.
+// If you disable the compiler directive above you will get a compilation error.
+// another option is to call the variable _* which allows it to be unused (similar to python).
+
+fn main() {
+    let _x: i32=0;
+    let x: i32=0;
+    println!("Hello World!");
+}
---
+++ src/core/unused_variable_underscore.rs
@@ -0,0 +1,7 @@
+// This is an example of how not to get an error from the compiler about an unused variable
+// by prefixing it with an underscore.
+
+fn main() {
+    let _x: i32=0;
+    println!("Hello World!");
+}
