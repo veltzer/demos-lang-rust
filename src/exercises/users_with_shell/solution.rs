caf8ca4dfbbe1bf23ee53efaa47252677bc94484
Mark Veltzer <mark.veltzer@gmail.com>
2023-05-20 17:52:25 +0300

    first exercise

---
+++ src/exercises/users_with_shell/solution.rs
@@ -0,0 +1,24 @@
+use std::fs::File;
+use std::io::*;
+
+/*
+ * This is a solution to the users exercise.
+ */
+
+fn main() {
+    let mut shell = String::new();
+    stdin().read_line(&mut shell).expect("Failed to read line");
+    shell = shell.strip_suffix("\n").unwrap().to_string();
+    // println!("shell is [{shell}]");
+    let file = File::open("/etc/passwd").unwrap();
+    let lines = BufReader::new(file).lines(); 
+    for line in lines {
+        let fline = line.unwrap();
+        let parts: Vec<&str> = fline.split(":").collect();
+        let current_shell = parts[6];
+        if current_shell==shell {
+            let username = parts[0];
+            println!("{username}");
+        }
+    }
+}
