b26066a0a4496c47fd10ab12157a327706c428c1
Mark Veltzer <mark.veltzer@gmail.com>
2023-05-20 16:20:07 +0300

    no commit message given

--- src/io/read_file_line_by_line.rs
+++ src/io/read_file_line_by_line.rs
@@ -1,5 +1,17 @@
 use std::fs::File;
+use std::io::*;
+
+/*
+ * This is an example of how to read a file line by line in rust
+ *
+ * Notes:
+ * - you must "unwrap" both the BufferedReader and the line to use them.
+ */
 
 fn main() {
-    let _file = File::open("/etc/passwd").unwrap();
+    let file = File::open("/etc/passwd").unwrap();
+    let lines = BufReader::new(file).lines(); 
+    for line in lines {
+        println!("{}", line.unwrap());
+    }
 }
