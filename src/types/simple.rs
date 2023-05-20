28d772e12e8f063a466005f71638419005319394
Mark Veltzer <mark.veltzer@gmail.com>
2023-05-20 22:33:50 +0300

    more types

--- src/types/simple.rs
+++ src/types/simple.rs
@@ -1,4 +1,7 @@
 // this example prints out various simple types of rust
+use std::io::stdout;
+use std::io::*;
+use std::fs::File;
 
 fn print_type_of<T>(_: &T) {
     println!("{}", std::any::type_name::<T>())
@@ -7,4 +10,9 @@ fn print_type_of<T>(_: &T) {
 fn main() {
     let x: i32=7;
     print_type_of(&x);
+    print_type_of(&stdout());
+    let file = File::open("/etc/passwd").unwrap();
+    print_type_of(&file);
+    let lines = BufReader::new(file).lines(); 
+    print_type_of(&lines);
 }
