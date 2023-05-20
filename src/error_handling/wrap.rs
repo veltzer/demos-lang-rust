2075380bcf24bc1ea0fc735d4c2d75ab737c8263
Mark Veltzer <mark.veltzer@gmail.com>
2023-05-20 22:22:39 +0300

    more examples

---
+++ src/error_handling/wrap.rs
@@ -0,0 +1,7 @@
+// This explains the .wrap() method of the Result type of Rust
+fn main() {
+    // Statements here are executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+}
