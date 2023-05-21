42d287d96e51697701a8c4985d80f695be3f3d49
Mark Veltzer <mark.veltzer@gmail.com>
2023-05-21 12:59:46 +0300

    no commit message given

---
+++ src/iterators/using_directly.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut sequence = 0..3;
+    println!("Four consecutive `next` calls on 0..3");
+    println!("> {:?}", sequence.next());
+    println!("> {:?}", sequence.next());
+    println!("> {:?}", sequence.next());
+    println!("> {:?}", sequence.next());
+}
