e40cdc16c4a7e07c2f771b81257ff76085936c54
Mark Veltzer <mark.veltzer@gmail.com>
2023-05-21 11:56:10 +0300

    no commit message given

--- src/loops/set_type.rs
+++ src/loops/set_type.rs
@@ -1,4 +1,6 @@
 // This is an example of how to set the type of the integer for range loops
+// References:
+// - https://stackoverflow.com/questions/59663036/allow-i-to-be-u64-in-a-for-loop-over-range
 
 fn main() {
     for n in 0..10u32 {
