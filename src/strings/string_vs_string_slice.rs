aa7bfb7c3db6fc5d7340e6887daf4e3a8d0fbc9c
Mark Veltzer <mark.veltzer@gmail.com>
2023-05-21 21:46:38 +0300

    no commit message given

--- src/strings/string_vs_string_slice.rs
+++ src/strings/string_vs_string_slice.rs
@@ -1,6 +1,7 @@
 // This example demonstrates the difference between "String"
 // and "StringSlice"
 // Strings own their memory, slices don't.
+// slices are just pointers, either to stack or to heap
 
 fn print_type_of<T>(_: &T) {
     println!("{}", std::any::type_name::<T>())
