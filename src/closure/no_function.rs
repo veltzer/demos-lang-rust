e9577594df9149264a479dfbcbcfd18fa253d7f7
Mark Veltzer <mark.veltzer@gmail.com>
2023-05-22 13:18:40 +0300

    no commit message given

--- src/closure/no_function.rs
+++ src/closure/no_function.rs
@@ -2,8 +2,9 @@
 // At first you may not understand what the fuss is all about...
 
 fn main() {
-    let c1 = |i| { i+3 };
-    let c2 = |i| { i+5 };
+    let m=5;
+    let c1 = |i| { i+3+m };
+    let c2 = |i| { i+5+m };
     let r1 = c1(6);
     let r2 = c2(6);
     println!("r1(6) is {r1}");
---
+++ src/types/overflow.rs
@@ -0,0 +1,6 @@
+// this example prints out various simple types of rust
+
+fn main() {
+    let x: u8=200;
+    println!("x is {x}");
+}
