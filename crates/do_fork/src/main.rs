f48c58f61c5c83e7707dbbad08231041b89e8a1c
Mark Veltzer <mark.veltzer@gmail.com>
2023-05-23 12:07:31 +0300

    no commit message given

---
+++ crates/do_fork/src/main.rs
@@ -0,0 +1,18 @@
+// This is an example of how to do fork in rust
+
+// References:
+// - https://docs.rs/fork/latest/fork/
+
+use fork::{daemon, Fork};
+use std::process::Command;
+
+
+fn main() {
+    println!("Hello from the parent");
+	if let Ok(Fork::Child) = daemon(false, false) {
+	   Command::new("sleep")
+		   .arg("3")
+		   .output()
+		   .expect("failed to execute process");
+	}
+}
