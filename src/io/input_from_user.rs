7dacbe2cfae11840d6576fa635ca5a74283f7042
Mark Veltzer <mark.veltzer@gmail.com>
2023-05-25 20:04:53 +0300

    more stuff

--- src/io/input_from_user.rs
+++ src/io/input_from_user.rs
@@ -1,6 +1,12 @@
-#[allow(unused_variables)]
+// This is a simple exapmle of how to read input from the user
+
+use std::io::{stdout,stdin,Write};
 
 fn main() {
     print!("What is your name: ");
-    let name: String = String::new();
+    stdout().flush().expect("canont flush stdout");
+    let mut name: String = String::new();
+    stdin().read_line(&mut name).expect("Did not enter a correct string");
+    name = name.strip_suffix("\n").unwrap().to_string();
+    println!("Your name is [{name}]...");
 }
