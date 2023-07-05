5b0163ea924e44e578c39654d1a8eb19d0bae967
Mark Veltzer <mark.veltzer@gmail.com>
2023-07-05 16:31:19 +0300

    no commit message given

---
+++ src/mutability/cant_change_var.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let x;
+    println!("hello");
+    //println!("The value of x is: {x}");
+    x=5;
+    println!("The value of x is: {x}");
+    //x = 6;
+    //println!("The value of x is: {x}");
+}
---
+++ src/mutability/inference.rs
@@ -0,0 +1,3 @@
+fn main() {
+    let _guess = "42".parse().expect("Not a number!");
+}
