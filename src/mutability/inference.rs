43b69f5506d29ffe4646f6e844a0aa25e0aab86d
Mark Veltzer <mark.veltzer@gmail.com>
2023-07-11 08:22:07 +0300

    no commit message given

--- src/mutability/inference.rs
+++ src/mutability/inference.rs
@@ -1,3 +1,8 @@
+/*
+ * In this example if you remove the explicit type given to _guess
+ * the example stops compiling
+ */
+
 fn main() {
     let _guess: u32 = "42".parse().expect("Not a number!");
 }
