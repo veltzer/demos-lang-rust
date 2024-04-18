36d3604813721d9a63d3f9dab7a3e7f1c7ed42dc
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-19 02:30:28 +0300

    no commit message given

--- config/deps.py
+++ config/deps.py
@@ -1,7 +1,8 @@
 packages=[
-    "rustc",  # this provides "rustdoc" as well as "rustc"
-    "cargo",
-    "cargo-binutils",
+    "rustc", # this provides "rustdoc" as well as "rustc"
+    "cargo", # manages projects in rust
     "rust-src", # for debugging support, browsing source code and vscode auto complete
-    "librust-cargo-binutils-dev",
+    # the two packages below do not exist on the CI/CD machines at github
+    # "cargo-binutils",
+    # "librust-cargo-binutils-dev",
 ]
