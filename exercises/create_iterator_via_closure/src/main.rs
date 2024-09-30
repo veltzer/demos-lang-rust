69087a31dd88fbcac7c478a8b17413a56bdc2ef1
Mark Veltzer <mark.veltzer@gmail.com>
2024-09-30 20:49:02 +0300

    another solution

--- exercises/create_iterator_via_closure/src/main.rs
+++ exercises/create_iterator_via_closure/src/main.rs
@@ -1,3 +1,20 @@
+fn create_iterator(start: i32, stop: i32, step: i32) -> impl Iterator<Item = i32> {
+    let mut current = start;
+    std::iter::from_fn(move || {
+        if current < stop {
+            let value = current;
+            current += step;
+            Some(value)
+        } else {
+            None
+        }
+    })
+}
+
 fn main() {
-    println!("Hello, world!");
+    let iterator = create_iterator(1, 10, 2);
+    
+    for value in iterator {
+        println!("{}", value);
+    }
 }
