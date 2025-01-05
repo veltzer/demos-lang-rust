4a5b23459ab7433d1c4bc21ac905f6db32705967
Mark Veltzer <mark.veltzer@gmail.com>
2025-01-05 16:13:23 +0200

    no commit message given

--- examples/closure/no_function/src/main.rs
+++ examples/closure/no_function/src/main.rs
@@ -2,11 +2,15 @@
 // At first you may not understand what the fuss is all about...
 
 fn main() {
-    let m=5;
-    let c1 = |i| { i+3+m };
-    let c2 = |i| { i+5+m };
+    let mut m=5;
+    let c1 = |i| { i+5+m };
+    let c2 = |i| { i+3+m };
     let r1 = c1(6);
     let r2 = c2(6);
-    println!("r1(6) is {r1}");
-    println!("r2(6) is {r2}");
+    m=6;
+    println!("c1(6) is {r1}");
+    println!("c2(6) is {r2}");
+    println!("c1(6) is {r1}");
+    println!("c2(6) is {r2}");
+    println!("m is {m}");
 }
