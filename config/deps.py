6574e814dc571ce0e5784aa32d1c7b8c55378fc2
Mark Veltzer <mark.veltzer@gmail.com>
2025-05-18 10:10:08 +0300

    added a few deps|

--- config/deps.py
+++ config/deps.py
@@ -7,6 +7,12 @@ packages=[
     "cargo", # manages projects in rust
     "rust-src", # for debugging support, browsing source code and vscode auto complete
     # the two packages below do not exist on the CI/CD machines at github
+    "clang",
+    "clang-18",
+    "lld",
+    "lld-18",
+    "cargo-doc",
+    "cargo-1.80-doc",
     # "cargo-binutils",
     # "librust-cargo-binutils-dev",
     # ruby stuff
